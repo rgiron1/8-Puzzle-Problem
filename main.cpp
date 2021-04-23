ce2ce0f0685517281e5a107fbbf04dc8d43b2fc5
ryan giron <rgiron1@yahoo.com>
2021-04-23 02:52:33 -0700

    added A* with misplaced tile heuristic

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <stack>
 #include <map>
 using namespace std;
-
+//-------------------------------------Priority Queue------------------------------------------------
 struct compareTotalCost{
     bool operator() (Node* a, Node* b){
         return (a->totalCost > b->totalCost);
@@ -16,15 +16,25 @@ struct compareTotalCost{
 map<vector<vector<int>>, bool> visited;
 priority_queue<Node*, vector<Node*>,  compareTotalCost> q;
 
-
-int AStarMisplaceHeuristic(vector<vector<int>> state){
+//---------------------------------------------------Misplaced tile heurisitc---------------------------------------------
+int AStarMisplaceHeuristic(vector<vector<int>> state, Problem p){
+    int heurisitc = 0;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(state[i][j] != p.goalState[i][j]){
+                heurisitc++;
+            }
+        }
+        return heurisitc;
+    }
     return 0;
 }
+//-----------------------------------------------------Eucldian distance heurisitc--------------------------------------------------
+int AStarEuclidianHeuristic(vector<vector<int>> state, Problem p){
 
-int AStarEuclidianHeuristic(vector<vector<int>> state){
     return 0;
 }
-
+//-----------------------------------------------------------------------Print--------------------------------------------------
 void Print(vector<vector<int>> input){
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
@@ -36,14 +46,13 @@ void Print(vector<vector<int>> input){
 }
 
 
-
-//UP FUNCTION
-void moveUp(vector<vector<int>> state, Node* n){
+//---------------------------------------------------------UP FUNCTION---------------------------------------------------------------------
+void moveUp(vector<vector<int>> state, Node* n, int heurisitc){
     if(n->zeroI > 0){
         vector<vector<int>> upState = state;
         swap(upState[n->zeroI][n->zeroJ], upState[n->zeroI - 1][n->zeroJ]);
         if(!visited.count(upState)){
-            Node *upNode = new Node(upState, n, 0);
+            Node *upNode = new Node(upState, n, heurisitc);
             upNode->zeroI = n->zeroI - 1;
             upNode->zeroJ = n->zeroJ;
             n->up = upNode;
@@ -59,13 +68,13 @@ void moveUp(vector<vector<int>> state, Node* n){
         // }
     } 
 }
-    //DOWN
-void moveDown(vector<vector<int>> state, Node* n){
+    //--------------------------------------------------------------------DOWN----------------------------------------------------------------------------
+void moveDown(vector<vector<int>> state, Node* n, int heurisitc){
     if(n->zeroI < 2){
         vector<vector<int>> downState = state;
         swap(downState[n->zeroI][n->zeroJ], downState[n->zeroI + 1][n->zeroJ]);
         if(!visited.count(downState)){
-            Node *downNode = new Node(downState, n, 0);
+            Node *downNode = new Node(downState, n, heurisitc);
             downNode->zeroI = n->zeroI + 1;
             downNode->zeroJ = n->zeroJ;
             n->down = downNode;
@@ -81,13 +90,13 @@ void moveDown(vector<vector<int>> state, Node* n){
         // }
     }
 }
-    //LEFT
-void moveLeft(vector<vector<int>> state, Node* n){
+    //--------------------------------------------------------------LEFT-----------------------------------------------------------------------
+void moveLeft(vector<vector<int>> state, Node* n, int heurisitc){
     if(n->zeroJ > 0){
         vector<vector<int>> leftState = state;
         swap(leftState[n->zeroI][n->zeroJ], leftState[n->zeroI][n->zeroJ - 1]);
         if(!visited.count(leftState)){
-            Node *leftNode = new Node(leftState, n, 0);
+            Node *leftNode = new Node(leftState, n, heurisitc);
             leftNode->zeroJ = n->zeroJ - 1;
             leftNode->zeroI = n->zeroI;
             n->left = leftNode;
@@ -103,13 +112,13 @@ void moveLeft(vector<vector<int>> state, Node* n){
         // }
     }
 }
-    //RIGHT
-void moveRight(vector<vector<int>> state, Node* n){
+    //--------------------------------------------------------------------RIGHT-------------------------------------------------------------------------
+void moveRight(vector<vector<int>> state, Node* n, int heurisitc){
     if(n->zeroJ < 2){
         vector<vector<int>> rightState = state;
         swap(rightState[n->zeroI][n->zeroJ], rightState[n->zeroI][n->zeroJ + 1]);
         if(!visited.count(rightState)){
-            Node *rightNode = new Node(rightState, n, 0);
+            Node *rightNode = new Node(rightState, n, heurisitc);
             rightNode->zeroJ = n->zeroJ + 1;
             rightNode->zeroI = n->zeroI;
             n->right = rightNode;
@@ -125,7 +134,7 @@ void moveRight(vector<vector<int>> state, Node* n){
         // }
     }
 }
-    
+//-------------------------------------------------------------BackTracing---------------------------------------------------------------
 void backTracing(Node* node) {
     int steps = 0;
     stack<vector<vector<int>>> stack;
@@ -140,7 +149,7 @@ void backTracing(Node* node) {
     }
     cout << "It took " << steps << " moves to solve the puzzle"<< endl;
 }
-
+//---------------------------------------------------Uniform Cost Search ----------------------------------------------------------
 bool uniformCostSearch(Problem p){
     Node *root = new Node(p.currentState);
     if(p.currentState == p.goalState){
@@ -156,30 +165,71 @@ bool uniformCostSearch(Problem p){
         }
     }
     
-    moveUp(p.currentState, root);
-    moveDown(p.currentState, root);
-    moveLeft(p.currentState, root);
-    moveRight(p.currentState, root);
+    moveUp(p.currentState, root, 0);
+    moveDown(p.currentState, root, 0);
+    moveLeft(p.currentState, root, 0);
+    moveRight(p.currentState, root, 0);
 
-//-----------------------------------------------------------------------QUEUE--------------------------------------------------------------------
     while(!q.empty()){
         Node* curr = q.top();
-        Print(curr->state);
+        //Print(curr->state);
         if(curr->state == p.goalState){
-            //backTracing(curr); //FOR BACKTRACKING AND EXTRA CREDIT
+            backTracing(curr); //FOR BACKTRACKING AND EXTRA CREDIT
             return true;
         }
         q.pop();
-        moveUp(curr->state, curr);
+        moveUp(curr->state, curr, 0);
         //DOWN
-        moveDown(curr->state, curr);
+        moveDown(curr->state, curr, 0);
         //LEFT
-        moveLeft(curr->state, curr);
+        moveLeft(curr->state, curr, 0);
         //RIGHT
-        moveRight(curr->state, curr);
+        moveRight(curr->state, curr, 0);
     }
     return false;
 }
+
+//---------------------------------------A * Misplaced tile herusitic -------------------------------------------------------------
+bool AStarMisplace(Problem p){
+    Node *root = new Node(p.currentState);
+    if(p.currentState == p.goalState){
+        return true;
+    }
+    visited[root->state] = true;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(p.currentState[i][j] == 0){
+                root->zeroI = i;
+                root->zeroJ = j;
+            }
+        }
+    }
+    
+    moveUp(p.currentState, root, AStarEuclidianHeuristic(p.currentState, p));
+    moveDown(p.currentState, root, AStarEuclidianHeuristic(p.currentState, p));
+    moveLeft(p.currentState, root, AStarEuclidianHeuristic(p.currentState, p));
+    moveRight(p.currentState, root, AStarEuclidianHeuristic(p.currentState, p));
+
+    while(!q.empty()){
+        Node* curr = q.top();
+        //Print(curr->state);
+        if(curr->state == p.goalState){
+            backTracing(curr); //FOR BACKTRACKING AND EXTRA CREDIT
+            return true;
+        }
+        q.pop();
+        moveUp(curr->state, curr, AStarEuclidianHeuristic(curr->state, p));
+        //DOWN
+        moveDown(curr->state, curr, AStarEuclidianHeuristic(curr->state, p));
+        //LEFT
+        moveLeft(curr->state, curr, AStarEuclidianHeuristic(curr->state, p));
+        //RIGHT
+        moveRight(curr->state, curr, AStarEuclidianHeuristic(curr->state, p));
+    }
+    return false;
+}
+
+
 
 int main(){
     cout << "Please enter a number between 1 - 6 to select the difficulty that the AI should solve." << endl;
@@ -196,7 +246,8 @@ int main(){
             cout << "INITIAL STATE" << endl;
             p.printCurrentState();
             cout << "------------------------------------" << endl;
-            success = uniformCostSearch(p);
+            //success = uniformCostSearch(p);
+            success = AStarMisplace(p);
             break;
         case 2: 
             p.initialState = p.veryEasyState;
@@ -204,7 +255,8 @@ int main(){
             cout << "INITIAL STATE" << endl;
             p.printCurrentState();
             cout << "------------------------------------" << endl;
-            success = uniformCostSearch(p);
+            //success = uniformCostSearch(p);
+            success = AStarMisplace(p);
             break;
         case 3: 
             p.initialState = p.easyState;
@@ -212,7 +264,8 @@ int main(){
             cout << "INITIAL STATE" << endl;
             p.printCurrentState();
             cout << "------------------------------------" << endl;
-            success = uniformCostSearch(p);
+            //success = uniformCostSearch(p);
+            success = AStarMisplace(p);
             break;
         case 4: 
             p.initialState = p.doableState;
@@ -220,7 +273,8 @@ int main(){
             cout << "INITIAL STATE" << endl;
             p.printCurrentState();
             cout << "------------------------------------" << endl;
-            success = uniformCostSearch(p);
+            //success = uniformCostSearch(p);
+            success = AStarMisplace(p);
             break;
         case 5: 
             p.initialState = p.ohBoyState;
@@ -228,7 +282,8 @@ int main(){
             cout << "INITIAL STATE" << endl;
             p.printCurrentState();
             cout << "------------------------------------" << endl;
-            success = uniformCostSearch(p);
+            //success = uniformCostSearch(p);
+            success = AStarMisplace(p);
             break;
         case 6: 
             p.initialState = p.impossibleState;
@@ -236,7 +291,8 @@ int main(){
             cout << "INITIAL STATE" << endl;
             p.printCurrentState();
             cout << "------------------------------------" << endl;
-            success = uniformCostSearch(p);
+            //success = uniformCostSearch(p);
+            success = AStarMisplace(p);
             break;
             
         default: 
