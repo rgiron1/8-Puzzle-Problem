0f602cae23233948c4634debec9128474ff2dd81
ryan giron <rgiron1@yahoo.com>
2021-04-24 18:27:36 -0700

    added last heuristic and finished additional options for inputs

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //#include "Tree.cpp"
 #include <string>
 #include <iostream>
+#include <algorithm>
 #include <queue>
 #include <stack>
 #include <map>
@@ -30,9 +31,32 @@ int AStarMisplaceHeuristic(vector<vector<int>> state, Problem p){
     return 0;
 }
 //-----------------------------------------------------Eucldian distance heurisitc--------------------------------------------------
-int AStarEuclidianHeuristic(vector<vector<int>> state, Problem p){
-
-    return 0;
+int AStarEuclidianHeuristic(vector<vector<int>> state){
+    int heuristic = 0;
+    for(int i = 0; i < state.size(); i++){
+        for(int j = 0; j < state[i].size(); j++){
+            if(state[i][j] == 1){
+                heuristic += sqrt(pow((i - 0),2) + pow((j - 0), 2));
+            } else if(state[i][j] == 2){
+                heuristic += sqrt(pow((i - 0),2) + pow((j - 1), 2));
+            } else if(state[i][j] == 3){
+                heuristic += sqrt(pow((i - 0),2) + pow((j - 2), 2));
+            } else if(state[i][j] == 4){
+                heuristic += sqrt(pow((i - 1),2) + pow((j - 0), 2));
+            } else if(state[i][j] == 5){
+                heuristic += sqrt(pow((i - 1),2) + pow((j - 1), 2));
+            } else if(state[i][j] == 6){
+                heuristic += sqrt(pow((i - 1),2) + pow((j - 2), 2));
+            } else if(state[i][j] == 7){
+                heuristic += sqrt(pow((i - 2),2) + pow((j - 0), 2));
+            } else if(state[i][j] == 8){
+                heuristic += sqrt(pow((i - 2),2) + pow((j - 1), 2));
+            } else if(state[i][j] == 0){
+                heuristic += sqrt(pow((i - 2),2) + pow((j - 2), 2));
+            }
+        }
+    }
+    return heuristic;
 }
 //-----------------------------------------------------------------------Print--------------------------------------------------
 void Print(vector<vector<int>> input){
@@ -147,12 +171,16 @@ void backTracing(Node* node) {
         Print(stack.top());
         stack.pop();
     }
-    cout << "It took " << steps << " moves to solve the puzzle"<< endl;
+    //cout << "It took " << steps << " moves to solve the puzzle"<< endl;
 }
 //---------------------------------------------------Uniform Cost Search ----------------------------------------------------------
 bool uniformCostSearch(Problem p){
+    int totalStates = 0;
+    int maxInQ = 0;
     Node *root = new Node(p.currentState);
+    totalStates++;
     if(p.currentState == p.goalState){
+        cout << "Total states expanded is: " << totalStates << endl;
         return true;
     }
     visited[root->state] = true;
@@ -172,9 +200,14 @@ bool uniformCostSearch(Problem p){
 
     while(!q.empty()){
         Node* curr = q.top();
-        //Print(curr->state);
+        maxInQ = max(maxInQ, int(q.size()));
+        totalStates++;
+        Print(curr->state);
         if(curr->state == p.goalState){
+            cout << endl << "STEPS TO GET TO SOLUTION: " << endl;
             backTracing(curr); //FOR BACKTRACKING AND EXTRA CREDIT
+            cout << "To solve this problem the search algorithm expanded a total of: " << totalStates << " nodes" << endl;
+            cout << "The maximum number of nodes in the queue at any one time: " << maxInQ << endl;
             return true;
         }
         q.pop();
@@ -191,7 +224,10 @@ bool uniformCostSearch(Problem p){
 
 //---------------------------------------A * Misplaced tile herusitic -------------------------------------------------------------
 bool AStarMisplace(Problem p){
+    int totalStates = 0;
+    int maxInQ = 0;
     Node *root = new Node(p.currentState);
+    totalStates++;
     if(p.currentState == p.goalState){
         return true;
     }
@@ -205,26 +241,78 @@ bool AStarMisplace(Problem p){
         }
     }
     
-    moveUp(p.currentState, root, AStarEuclidianHeuristic(p.currentState, p));
-    moveDown(p.currentState, root, AStarEuclidianHeuristic(p.currentState, p));
-    moveLeft(p.currentState, root, AStarEuclidianHeuristic(p.currentState, p));
-    moveRight(p.currentState, root, AStarEuclidianHeuristic(p.currentState, p));
+    moveUp(p.currentState, root, AStarMisplaceHeuristic(p.currentState, p));
+    moveDown(p.currentState, root, AStarMisplaceHeuristic(p.currentState, p));
+    moveLeft(p.currentState, root, AStarMisplaceHeuristic(p.currentState, p));
+    moveRight(p.currentState, root, AStarMisplaceHeuristic(p.currentState, p));
 
     while(!q.empty()){
         Node* curr = q.top();
-        //Print(curr->state);
+        maxInQ = max(maxInQ, int(q.size()));
+        totalStates++;
+        Print(curr->state);
         if(curr->state == p.goalState){
+            cout << endl << "STEPS TO GET TO SOLUTION: " << endl;
             backTracing(curr); //FOR BACKTRACKING AND EXTRA CREDIT
+            cout << "To solve this problem the search algorithm expanded a total of: " << totalStates << " nodes" << endl;
+            cout << "The maximum number of nodes in the queue at any one time: " << maxInQ << endl;
             return true;
         }
         q.pop();
-        moveUp(curr->state, curr, AStarEuclidianHeuristic(curr->state, p));
+        moveUp(curr->state, curr, AStarMisplaceHeuristic(curr->state, p));
         //DOWN
-        moveDown(curr->state, curr, AStarEuclidianHeuristic(curr->state, p));
+        moveDown(curr->state, curr, AStarMisplaceHeuristic(curr->state, p));
         //LEFT
-        moveLeft(curr->state, curr, AStarEuclidianHeuristic(curr->state, p));
+        moveLeft(curr->state, curr, AStarMisplaceHeuristic(curr->state, p));
         //RIGHT
-        moveRight(curr->state, curr, AStarEuclidianHeuristic(curr->state, p));
+        moveRight(curr->state, curr, AStarMisplaceHeuristic(curr->state, p));
+    }
+    return false;
+}
+//------------------------------------------A* Euclidean distance------------------------------------------
+bool AStarEuclidean(Problem p){
+    int totalStates = 0;
+    int maxInQ = 0;
+    Node *root = new Node(p.currentState);
+    totalStates++;
+    if(p.currentState == p.goalState){
+        return true;
+    }
+    visited[root->state] = true;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(p.currentState[i][j] == 0){
+                root->zeroI = i;
+                root->zeroJ = j;
+            }
+        }
+    }
+    
+    moveUp(p.currentState, root, AStarEuclidianHeuristic(p.currentState));
+    moveDown(p.currentState, root, AStarEuclidianHeuristic(p.currentState));
+    moveLeft(p.currentState, root, AStarEuclidianHeuristic(p.currentState));
+    moveRight(p.currentState, root, AStarEuclidianHeuristic(p.currentState));
+
+    while(!q.empty()){
+        Node* curr = q.top();
+        maxInQ = max(maxInQ, int(q.size()));
+        totalStates++;
+        Print(curr->state);
+        if(curr->state == p.goalState){
+            cout << endl << "STEPS TO GET TO SOLUTION: " << endl;
+            backTracing(curr); //FOR BACKTRACKING AND EXTRA CREDIT
+            cout << "To solve this problem the search algorithm expanded a total of: " << totalStates << " nodes" << endl;
+            cout << "The maximum number of nodes in the queue at any one time: " << maxInQ << endl;
+            return true;
+        }
+        q.pop();
+        moveUp(curr->state, curr, AStarEuclidianHeuristic(curr->state));
+        //DOWN
+        moveDown(curr->state, curr, AStarEuclidianHeuristic(curr->state));
+        //LEFT
+        moveLeft(curr->state, curr, AStarEuclidianHeuristic(curr->state));
+        //RIGHT
+        moveRight(curr->state, curr, AStarEuclidianHeuristic(curr->state));
     }
     return false;
 }
@@ -232,74 +320,108 @@ bool AStarMisplace(Problem p){
 
 
 int main(){
-    cout << "Please enter a number between 1 - 6 to select the difficulty that the AI should solve." << endl;
+    cout << "Please enter a number between 1 - 6 to select the difficulty that the AI should solve, or press 7 to make your own puzzle." << endl;
     int difficulty = 0;
     bool success = false;
     cin >> difficulty;
     Problem p;
+
+    string first = "";
+    string second = "";
+    string third = "";
+    vector<int> firstRow;
+    vector<int> secondRow;
+    vector<int> thridRow;
+    vector<vector<int>> inputProb;
     
     
     switch(difficulty){
         case 1: 
             p.initialState = p.trivialState;
             p.setCurrentState(p.initialState);
-            cout << "INITIAL STATE" << endl;
-            p.printCurrentState();
-            cout << "------------------------------------" << endl;
-            //success = uniformCostSearch(p);
-            success = AStarMisplace(p);
             break;
         case 2: 
             p.initialState = p.veryEasyState;
             p.setCurrentState(p.initialState);
-            cout << "INITIAL STATE" << endl;
-            p.printCurrentState();
-            cout << "------------------------------------" << endl;
-            //success = uniformCostSearch(p);
-            success = AStarMisplace(p);
             break;
         case 3: 
             p.initialState = p.easyState;
             p.setCurrentState(p.initialState);
-            cout << "INITIAL STATE" << endl;
-            p.printCurrentState();
-            cout << "------------------------------------" << endl;
-            //success = uniformCostSearch(p);
-            success = AStarMisplace(p);
             break;
         case 4: 
             p.initialState = p.doableState;
             p.setCurrentState(p.initialState);
-            cout << "INITIAL STATE" << endl;
-            p.printCurrentState();
-            cout << "------------------------------------" << endl;
-            //success = uniformCostSearch(p);
-            success = AStarMisplace(p);
             break;
         case 5: 
             p.initialState = p.ohBoyState;
             p.setCurrentState(p.initialState);
-            cout << "INITIAL STATE" << endl;
-            p.printCurrentState();
-            cout << "------------------------------------" << endl;
-            //success = uniformCostSearch(p);
-            success = AStarMisplace(p);
             break;
         case 6: 
             p.initialState = p.impossibleState;
             p.setCurrentState(p.initialState);
-            cout << "INITIAL STATE" << endl;
-            p.printCurrentState();
-            cout << "------------------------------------" << endl;
-            //success = uniformCostSearch(p);
-            success = AStarMisplace(p);
             break;
-            
+        case 7: 
+            cout << "Please enter the first row: ";
+            cin >> first;
+            cout << "Please enter the second row: ";
+            cin >> second;
+            cout << "Please enter the third row: ";
+            cin >> third;
+
+            for(auto x : first){
+                firstRow.push_back(int(x) - 48);
+            } 
+            inputProb.push_back(firstRow);
+
+            for(auto x : second){
+                secondRow.push_back(int(x) - 48);
+            } 
+            inputProb.push_back(secondRow);
+
+            for(auto x : third){
+                thridRow.push_back(int(x) - 48);
+            } 
+            inputProb.push_back(thridRow);
+            p.initialState = inputProb;
+            p.setCurrentState(p.initialState);
+
+        break;
+
         default: 
         cout << "Please rerun the program and enter a correct number" << endl;
             break;
     }
-    
+
+    cout << "Please enter the the number of the algorithm" << endl << "1. Uniform Cost Search" << endl << "2. A* Misplaced Tile Heurisitc" << endl << "3. A* Euclidean Distance Heurisitc" << endl;
+    int algo = 0;
+    cin >> algo;
+
+    switch (algo)
+    {
+    case 1:
+         cout << "INITIAL STATE" << endl;
+            p.printCurrentState();
+            cout << "------------------------------------" << endl;
+            success = uniformCostSearch(p);
+        break;
+    case 2:
+            cout << "INITIAL STATE" << endl;
+            p.printCurrentState();
+            cout << "------------------------------------" << endl;
+            success = AStarMisplace(p);
+        break;
+    case 3:  
+            cout << "INITIAL STATE" << endl;
+            p.printCurrentState();
+            cout << "------------------------------------" << endl;
+            success = AStarEuclidean(p);
+        break;
+
+    default:
+        cout << "Please rerun the program and enter a correct number" << endl;
+        break;
+    }
+
     if(success){
         cout << "SUCCESS" << endl;
     } else {
